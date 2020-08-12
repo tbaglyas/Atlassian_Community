b9634b3f2bfa464203ba3fa7b22ff777b9a286d6
Tamás Baglyas <tamas.baglyas@gmail.com>
2020-08-12 07:18:48 +0200

    Tamás Baglyas's answer - clarified version

--- Jira/Groovy/Hide field with Behaviours.groovy
+++ Jira/Groovy/Hide field with Behaviours.groovy
@@ -1,11 +1,11 @@
 /**
 * Atlassian Community answer
 * <a href="https://community.atlassian.com/t5/Jira-questions/Adaptavist-ScriptRunner-Behaviour-Hide-field-based-on-value-of/qaq-p/1453585" target="_blank">Adaptavist ScriptRunner Behaviour - Hide field based on value of other field</a>
-* @author   Tamás Baglyas - https://github.com/tbaglyas
-* @version  1.0
-* @since    2020-08-11
+* @author Tamás Baglyas - https://github.com/tbaglyas
+* @version 1.1
+* @since 2020-08-11
 */
-if (getActionName().equals("Transition name where you want to use this behaviours")) {
-    boolean visible = getFieldById("customfield_10907")?.getValue()?.toString()?.equals("Training");
-    getFieldById("customfield_28501").setHidden(visible);
+if (getActionName().equals("Transition name where you want to use these behaviours.")) {
+    boolean hidden = !(getFieldById("customfield_10907")?.getValue()?.toString()?.equals("Training"));
+    getFieldById("customfield_28501").setHidden(hidden);
 }
